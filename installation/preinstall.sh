fa3da1968b5213ae223d06b79b48d0ed6651cce2
Ionică Bizău <bizauionica@yahoo.com>
2014-04-03 12:21:58 +0300

    Download the needed json files from the GitHub organization

---
+++ installation/preinstall.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/BibleJS/Versions/v0.1.0/RO/json/books.json   -P bibles/RO/
+wget https://raw.githubusercontent.com/BibleJS/Versions/v0.1.0/RO/json/verses.json  -P bibles/RO/
