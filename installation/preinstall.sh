340ec5ce089a40d3cc37ad44af338cdd2e68d575
Ionică Bizău <bizauionica@yahoo.com>
2014-04-04 13:00:42 +0300

    Upgrade to Bible data v0.1.3

--- installation/preinstall.sh
+++ installation/preinstall.sh
@@ -1,2 +1,1 @@
-wget https://raw.githubusercontent.com/BibleJS/Versions/v0.1.1/RO/json/books.json   -P bibles/RO/
-wget https://raw.githubusercontent.com/BibleJS/Versions/v0.1.1/RO/json/verses.json  -P bibles/RO/
+wget https://raw.githubusercontent.com/BibleJS/Versions/v0.1.3/RO/json/verses.json  -P bibles/RO/
