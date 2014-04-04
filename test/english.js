ca7546b8ccd2e27525fbb54fca6bfcfbbffefa53
Ionică Bizău <bizauionica@yahoo.com>
2014-04-04 09:36:29 +0300

    Add some separators

--- test/english.js
+++ test/english.js
@@ -1,12 +1,18 @@
 // dependencies
 var Bible = new (require ("../index"))({
     language: "EN"
-});
+})
+  , reference = "Psalm 1:1-6"
+  ;
+
+console.log(reference);
+console.log("-------------");
 
 // get verse
 Bible.get("Psalm 1:1-6", function (err, data) {
     if (data && data.length) {
         for (var i in data)
         console.log(data[i].verse + " | " + data[i].text);
+        console.log("-------------\n");
     }
 });
