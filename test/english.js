8122473df1d534de3c096d3888563114c6e72ab0
Ionică Bizău <bizauionica@yahoo.com>
2014-04-04 09:37:00 +0300

    Use reference to get the Bible verses

--- test/english.js
+++ test/english.js
@@ -9,7 +9,7 @@ console.log(reference);
 console.log("-------------");
 
 // get verse
-Bible.get("Psalm 1:1-6", function (err, data) {
+Bible.get(reference, function (err, data) {
     if (data && data.length) {
         for (var i in data)
         console.log(data[i].verse + " | " + data[i].text);
