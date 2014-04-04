2629eebe698d5b4983a294aacfcaff78e4d63549
Ionică Bizău <bizauionica@yahoo.com>
2014-04-04 09:39:48 +0300

    Comments

--- test/english.js
+++ test/english.js
@@ -2,17 +2,29 @@
 var Bible = new (require ("../index"))({
     language: "EN"
 })
+
+    // the Bible reference
   , reference = "Psalm 1:1-6"
   ;
 
+// output
 console.log(reference);
 console.log("-------------");
 
 // get verse
 Bible.get(reference, function (err, data) {
+
+    // we've get the verses
     if (data && data.length) {
-        for (var i in data)
-        console.log(data[i].verse + " | " + data[i].text);
+
+        // each verse
+        for (var i in data) {
+
+            // output
+            console.log(data[i].verse + " | " + data[i].text);
+        }
+
+        // output
         console.log("-------------\n");
     }
 });
