67e62194ae04ec460bd9cbaa7fe971210ae97993
Ionică Bizău <bizauionica@yahoo.com>
2014-07-25 10:28:03 +0300

    Added test/index.js

---
+++ test/index.js
@@ -0,0 +1,52 @@
+// Dependencies
+var Bible = require("../index");
+
+// Init Bible
+Bible.init({
+    versions: {
+        en: {
+            source: "https://github.com/BibleJS/bible-english"
+          , version: "master"
+          , language: "en"
+        },
+        ro: {
+            source: "https://github.com/BibleJS/bible-romanian"
+          , version: "master"
+          , language: "en"
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+
+    // Create Bible instances
+    var enBible = new Bible({ language: "en" })
+      , roBible = new Bible({ language: "ro" })
+      , references = {
+            en: "Psalm 1:1-6"
+          , ro: "Psalmi 1:1-6"
+        }
+      , responseHandler = function (lang) {
+            return function (err, data) {
+                if (err) { throw err; }
+
+                // Output
+                console.log(references[lang]);
+                console.log("-------------");
+
+                if (data && data.length) {
+
+                    // each verse
+                    for (var i in data) {
+                        console.log(data[i].verse + " | " + data[i].text);
+                    }
+
+                    console.log("-------------\n");
+                }
+            }
+        }
+      ;
+
+    // Get verses
+    enBible.get(references.en, responseHandler("en"));
+    roBible.get(reference.ro, responseHandler("ro"));
+});
